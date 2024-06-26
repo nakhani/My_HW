5ba9ff0276e11c77c887f567a02dab0a86ec8396
najmeh <najmehkhani.95@gmail.com>
2024-06-26 08:27:52 -0700

    complete HW

--- session11/carpet.py
+++ session11/carpet.py
@@ -1,19 +1,31 @@
-import random
-list=["🎀","🌸","☘","🍁","🍂","🌼","🌻","🌺","🍄","🌴"]
-      
 def carpet(n):
-    for i in range (1, n):
-        for j in range (1, n):
-         x= random.choice(list)
-         print(x, end = " ")
+    if n % 2 == 0:
+        print("Please enter an odd number.")
+        return
+    outer_symbol = "🔘"
+    middle_symbol1 = "🟣"
+    middle_symbol2 = "🔵"
+    middle_symbol3 = "🟤"
+    middle_symbol4 = "🟡"
+    middle_symbol5 = "🟠"
+
+    for i in range(n):
+        for j in range(n):
+            if i == 0 or i == n-1 or j == 0 or j == n-1:
+                print(outer_symbol, end=' ')
+            elif i == 1 or i == n-2 or j == 1 or j == n-2:
+                print(middle_symbol1, end=' ')
+            elif i == 2 or i == n-3 or j == 2 or j == n-3:
+                print(middle_symbol2, end=' ')
+            elif i == 3 or i == n-4 or j == 3 or j == n-4:
+                print(middle_symbol3, end=' ')
+            elif i == 4 or i == n-5 or j == 4 or j == n-5:
+                print(middle_symbol5, end=' ')
+            elif i == (n//2) and j == (n//2):
+                print(middle_symbol4, end=' ')
+            else:
+                print(middle_symbol3, end=' ')
         print()
 
-        
-number=int(input("Please enter your number?"))
-
-if number%2==0 :
-    print("This is just python Homework")
-
-else:
-    carpet(number)
-    
+number = int(input("Enter your number: "))
+carpet(number)
